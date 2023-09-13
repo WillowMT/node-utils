de99023e8459e430d6c61f0940b5117c5d030d10
WillowMT <waiyanmt.20@icloud.com>
2023-09-14 00:38:01 +0800

    first commit

---
+++ commands.ts
@@ -0,0 +1,30 @@
+interface template {
+    packages: string[];
+    commands: string[];
+}
+
+export const jest: template = {
+    packages: ["jest", "babel-jest", "@babel/coreD", "@babel/preset-env"],
+    commands: ['npm pkg set scripts.test="jest"'],
+};
+
+export const jest_ts: template = {
+    packages: [
+        "ts-jest",
+        "babel-jest",
+        "@babel/coreD",
+        "@babel/preset-env",
+        "@types/jest",
+    ],
+    commands: ['npm pkg set scripts.test="jest"'],
+};
+
+export const husky: template = {
+    packages: ["husky"],
+    commands: [
+        'npm pkg set scripts.prepare="husky install"',
+        "npm run prepare",
+        'npx husky add .husky/pre-commit "npm test"',
+        "git add .husky/pre-commit",
+    ],
+};
---
+++ index.ts
@@ -0,0 +1,9 @@
+import {exec} from 'node:child_process'
+import * as c from './commands'
+import * as p from '@clack/prompts'
+
+async function main() {
+
+}
+
+main()
